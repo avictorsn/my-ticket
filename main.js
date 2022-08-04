d98c9cf923bafc13f5a0752d34f97bceca023525
Victor Santiago <antoniovictor@ymail.com>
2022-08-04 19:03:13 -0300

    [create-app] Adicionando scripts de build e conceito de IPC

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const {app, BrowserWindow, Menu, Tray, ipcMain} = require('electron');
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
 
 let window;
 
@@ -18,10 +22,64 @@ app.whenReady().then(()=> {
             contextIsolation: false
         }
     };
-    createWindow(window, optionsWindow1, 'http://localhost:4200');
+    const _url = app.isPackaged ?  
+    url.format({
+        pathname: path.join(__dirname, `dist/my-ticket/index.html`),
+        protocol: "file:",
+        slashes: true
+    })  : 'http://localhost:4200'
+
+    createWindow(window, optionsWindow1, _url);
 
 })
 
-ipcMain.on('click-menu', ()=> {
-  console.log('CLICK');
+function setClickCounter(counter) {
+    const _path = mountDataPath('click_counter')
+
+    try {
+        fs.writeFileSync(_path, JSON.stringify({clicks: counter}), 'utf-8');
+    } catch (error) {
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
+    }
+}
+
+function getClickCounter() {
+    const _path = mountDataPath('click_counter')
+
+    try {
+        if(fs.existsSync(_path)) {
+            return JSON.parse(fs.readFileSync(_path));
+        }
+    } catch (error) {
+        console.log('====================================');
+        console.log(error);
+        console.log('====================================');
+    }
+}
+
+const mountDataPath = (newFilename) => {
+    const userDataPath = app.getPath('userData');
+    let newPath = path.join(userDataPath, `${newFilename}.json`);
+
+    return newPath;
+}
+
+ipcMain.on('click-menu', (event, {clicks})=> {
+    // Incrementa o counter
+    let newClickCounter = clicks + 1;
+
+    // Escreve os dados em JSON
+    setClickCounter(newClickCounter);
+
+    // Devolve o valor incrementado pro renderer process
+    event.sender.send('already-clicked', {clicks: newClickCounter})
+})
+
+ipcMain.on('recover-counter', (event)=> {
+    let output = getClickCounter() ? getClickCounter(): {clicks: 0};
+    console.log(output);
+
+    event.sender.send('get-counter', output);
 })
--- src/app/board/components/boards-menu/boards-menu.component.ts
+++ src/app/board/components/boards-menu/boards-menu.component.ts
@@ -8,13 +8,28 @@ import { IpcService } from 'src/app/ipc.service';
 })
 export class BoardsMenuComponent implements OnInit {
 
-  constructor(private ipcRenderer: IpcService) { }
+  public clickCounter: number = 0;
+
+  constructor(private ipcService: IpcService) {
+  }
 
   ngOnInit(): void {
+    this.ipcService._ipc?.on('already-clicked', (event, args)=> {
+      this.clickCounter = args.clicks;
+    })
+
+    // Solicitar dados do JSON do click
+    this.ipcService._ipc?.send('recover-counter');
+
+    // Escutar dados recebidos do JSON e renderizar em tela
+    this.ipcService._ipc?.on('get-counter', (event, args)=> {
+      this.clickCounter = args.clicks;
+    });
+
   }
 
   onClickMenu() {
-    this.ipcRenderer._ipc?.send('click-menu')
+    this.ipcService._ipc?.send('click-menu', {clicks: this.clickCounter})
   }
 
 }
